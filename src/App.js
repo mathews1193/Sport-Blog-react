1b2b38c557e99a330beeaff76020c6e61cc846ff
mathews1193 <mathews1193@yahoo.com>
2020-11-02 11:21:38 -0600

    added  a react router and changed title

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-     <h1>Hello</h1> 
+class App extends React.Component {
+  render(){
+    return (
+    <div>
+      <div className="container">
+          <Router>
+            <Switch>
+            </Switch>
+          </Router> 
+      </div>
     </div>
   );
+  }
 }
+  
 
 export default App;
